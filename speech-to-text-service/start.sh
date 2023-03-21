1026b77662410f7ee75ee1481148a4cdefc3a9d5
Jirawat-rackz <Jirawat_ku@kkumail.com>
2023-03-21 15:52:45 +0700

    feat: change sh file script

--- speech-to-text-service/start.sh
+++ speech-to-text-service/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-FILE=model.tflite
-if ! [ -f "$FILE" ]; then
+FILE=./model.tflite
+if [ ! -f "$FILE" ]; then
     wget https://coqui.gateway.scarf.sh/english/coqui/v1.0.0-large-vocab/model.tflite
 fi
 
-FILE=large_vocabulary.scorer
-if ! [ -f "$FILE" ]; then
+FILE=./large_vocabulary.scorer
+if [ ! -f "$FILE" ]; then
     wget https://coqui.gateway.scarf.sh/english/coqui/v1.0.0-large-vocab/large_vocabulary.scorer
 fi
 
