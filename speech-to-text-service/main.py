9e0cca7b94afd1f216eac45a95bc09f91815cb02
Jirawat-rackz <Jirawat_ku@kkumail.com>
2023-03-26 01:36:10 +0700

    feat: add new stt service

--- speech-to-text-service/main.py
+++ speech-to-text-service/main.py
@@ -1,14 +1,13 @@
 import json
-from stt import Model
+import uuid
 from scipy.io import wavfile
-
 import scipy.signal as sps
 import os
 from paho.mqtt import client as mqtt_client
 import numpy as np
 import base64
-import uuid
-from os.path import dirname, join as pjoin
+import openai
+
 
 # FOR TESTING LOCAL ONLY
 # take environment variables from .env
@@ -17,6 +16,7 @@ from os.path import dirname, join as pjoin
 
 if __name__ == "__main__":
 
+    openai.api_key = os.getenv("OPENAI_API_KEY")
     # STT ENV
     SCORER_PATH = os.environ.get("STT_SCORER_PATH")
     MODEL_PATH = os.environ.get("STT_MODEL_PATH")
@@ -27,66 +27,54 @@ if __name__ == "__main__":
     MQTT_TOPIC_SUB = os.environ.get("MQTT_TOPIC_SUB")
     MQTT_TOPIC_PUB = os.environ.get("MQTT_TOPIC_PUB")
 
-    # init model
-    ds = Model(MODEL_PATH)
-    ds.enableExternalScorer(SCORER_PATH)
-    desired_sample_rate = ds.sampleRate()
-
     def run_stt(base64_data):
         # Decode the Base64 string
         decoded_data = base64.b64decode(base64_data)
         filename = uuid.uuid4().hex + "-temp.wav"
-        wav_fname = pjoin("./", filename)
-
-        # pad the binary data with zeros to make it a multiple of the size of a 16-bit integer
-        padded_data = decoded_data.ljust(
-            len(decoded_data) + len(decoded_data) % 2, b"\x00"
-        )
+        wav_fname = "./" + filename
 
         # convert the binary data to a numpy array
-        audio_array = np.frombuffer(padded_data, dtype=np.int16)
+        audio_array = np.frombuffer(decoded_data, dtype=np.int16)
 
         # write the numpy array to a WAV file
         wavfile.write(wav_fname, 44100, audio_array)
 
         # read the WAV file using the wavfile.read() function
-        sampling_rate, data = wavfile.read(wav_fname)
+        # sampling_rate, data = wavfile.read("./yes_1000ms.wav")
 
-        # Resample data
-        number_of_samples = round(
-            len(data) * float(desired_sample_rate) / sampling_rate
-        )
+        # # Resample data
+        # number_of_samples = round(
+        #     len(data) * float(desired_sample_rate) / sampling_rate
+        # )
 
-        data = sps.resample(data, number_of_samples).astype("int16")
-        os.remove(filename)
-        return ds.stt(data)
+        # data = sps.resample(data, number_of_samples).astype("int16")
+        audio_file = open(wav_fname, "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        audio_file.close()
+
+        # remove temp file
+        os.remove(wav_fname)
+        return transcript["text"]
 
     # init MQTT
     def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+        print("Connected with result code " + str(rc))
 
     def on_message(client, userdata, msg):
-
         """
         ----Subscribe msg model----
         user_id : string
         data:     string
         """
         decode = json.loads(msg.payload)
+        base_data = decode["data"].split(",")[1]
+        # wav_data = base64.b64decode(base_data)
 
-        print("Accept message: ", decode["user_id"])
-        base_data = decode["data"].split("data:audio/wav;base64,")[1]
+        print("User: ", decode["user_id"])  # for testing
 
-        # Read the .wav file
         text = run_stt(base_data)
 
-        print("Transcript: ", text)
-
-        # Delete the .wav file
-        # os.remove(filename)
+        print("Text: ", text)  # for testing
 
         """
         ----Publish msg model----
@@ -96,9 +84,10 @@ if __name__ == "__main__":
         pub_data = {"user_id": decode["user_id"], "word": text}
         client.publish(MQTT_TOPIC_PUB, json.dumps(pub_data))
 
-    client = mqtt_client.Client()
+    client = mqtt_client.Client("stt-service")
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(MQTT_URL, int(MQTT_PORT))
-    client.subscribe((MQTT_TOPIC_SUB, 0))
+    client.subscribe(MQTT_TOPIC_SUB)
+
     client.loop_forever()
